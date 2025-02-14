c938aac7acaa869770c6e57169fb97b72d4f2854
Xinxiang <angelowxx@gmail.com>
2025-02-14 21:14:23 +0100

    update 14.02

--- src/train.py
+++ src/train.py
@@ -132,7 +132,7 @@ def train_generator(generator, discriminator, lr_imgs, hr_imgs, criterion, g_opt
     # 或者改成按概率决定 sigma = Norm(g_loss, pre_loss**2), if sigma > pre_loss
     sigma = torch.normal(mean=g_loss, std=pre_loss ** 2)  # 生成 sigma
     if sigma > pre_loss:
-        g_loss = criterion(sr_images.detach(), pre_sr_imgs)
+        g_loss = criterion(sr_images, pre_sr_imgs.detach())
 
     else:
         g_loss = g_loss + criterion(sr_images * 5, hr_imgs * 5)
