862fee208e77236756ead6c4a8d40f27a4a3c867
Xinxiang <angelowxx@gmail.com>
2025-02-14 19:17:49 +0100

    update 14.02

--- src/variables.py
+++ src/variables.py
@@ -2,5 +2,5 @@ train_data_path = r'D:\files\phtographing\split_images'
 test_data_path = r'D:\files\phtographing\test_images'
 kaggle_data_path = r'C:\Users\mi\.cache\kagglehub\datasets\alessiocorrado99\animals10\versions\2\raw-img\cane'
 videos_save_path = r'/content/drive/MyDrive/image_repairing_CNN'
-clip_width = 200
-clip_height = 100
+clip_width = 1920
+clip_height = 820
