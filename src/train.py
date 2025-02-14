a8db3e5d22b995318fae2c73b6911ae2a53e637e
Xinxiang <angelowxx@gmail.com>
2025-02-15 00:22:58 +0100

    update 14.02

--- src/train.py
+++ src/train.py
@@ -199,8 +199,8 @@ def validate(model, val_loader, device, epoch, num_models):
 
 if __name__ == "__main__":
     train_example(2, 1)
-    train_example(1, 3)
-    train_example(1, 5)
+    train_example(2, 3)
+    train_example(2, 5)
 
     train_example(20, 1)
     train_example(20, 3)
