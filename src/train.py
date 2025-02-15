a1b578e15614aa3790088e298c84827b2e7086a6
xinxiang wang <angelowxx@gmail.com>
2025-02-15 06:05:51 +0000

    updating from colab

--- src/train.py
+++ src/train.py
@@ -27,7 +27,7 @@ def train_example(num_epochs, num_models):
     discriminator = Discriminator().to(device)
     model = [SRResNet().to(device) for i in range(num_models)]
     optimizer = [optim.Adam(generator.parameters(), lr=0.001) for generator in model]
-    d_optimizer = optim.Adam(discriminator.parameters(), lr=0.001)
+    d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0001)
     scheduler = optim.lr_scheduler.CosineAnnealingLR
     d_lr_scheduler = scheduler(
         optimizer=d_optimizer,
@@ -212,6 +212,6 @@ def validate(model, val_loader, device, epoch, num_models):
 
 if __name__ == "__main__":
 
-    train_example(20, 1)
     train_example(20, 3)
+    train_example(20, 1)
     train_example(20, 5)
