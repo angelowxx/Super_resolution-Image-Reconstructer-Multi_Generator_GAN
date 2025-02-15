1d78b443575bf2be4284a1a17d83d4eb71375f13
Xinxiang <angelowxx@gmail.com>
2025-02-15 15:50:59 +0100

    update 15.02

--- src/models.py
+++ src/models.py
@@ -106,8 +106,8 @@ class Discriminator(nn.Module):
             nn.BatchNorm1d(num_filters),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Linear(num_filters, 1),
-            nn.Sigmoid()
+            nn.Linear(num_filters, 2),
+            nn.Softmax(1)
         )
 
     def forward(self, x):
