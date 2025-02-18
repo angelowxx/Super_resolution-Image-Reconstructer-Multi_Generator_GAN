3b2926c4efeddf9f2a74ef01eca2057fdb4258ab
Xinxiang <angelowxx@gmail.com>
2025-02-18 19:31:11 +0100

    update 18.2

--- src/train.py
+++ src/train.py
@@ -20,7 +20,7 @@ import torchvision.utils as vutils
 
 import torch.nn.functional as F
 
-nums_epoch = 5
+nums_epoch = 20
 
 
 def train_example(rank, world_size, num_epochs):
@@ -68,7 +68,7 @@ def train_example(rank, world_size, num_epochs):
     train_data = ImageDatasetWithTransforms(train_folder_path, normalize_img_size, downward_img_quality)
 
     # Define split sizes (e.g., 70% train, 30% validation)
-    split_ratio = 0.1
+    split_ratio = 0.8
     train_size = int(split_ratio * len(train_data))
     val_size = len(train_data) - train_size
 
