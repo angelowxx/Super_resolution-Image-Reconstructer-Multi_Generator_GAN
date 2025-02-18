e48c880625d81c5477b26029c3633e3c5144f601
Xinxiang <angelowxx@gmail.com>
2025-02-18 22:49:03 +0100

    update 18.2

--- src/train.py
+++ src/train.py
@@ -179,9 +179,9 @@ def train_generator(generator, image_finger_print, discriminator, lr_imgs, hr_im
         real_preds = discriminator(hr_imgs)
 
     # g_loss = g_criterion(fake_prints, real_prints) + g_criterion(sr_images, hr_imgs) + torch.mean(real_preds-fake_preds)
-    # g_loss = torch.mean(real_preds-fake_preds)
+    g_loss = torch.mean(real_preds-fake_preds)
     # g_loss = g_criterion(fake_prints, real_prints)
-    g_loss = g_criterion(sr_images, hr_imgs)
+    # g_loss = g_criterion(sr_images, hr_imgs)
 
     g_optimizer.zero_grad()
     g_loss.backward()
