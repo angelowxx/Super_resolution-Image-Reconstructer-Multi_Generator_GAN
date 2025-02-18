ee4e79c3e390fda84438981abf4a052f41487369
Xinxiang <angelowxx@gmail.com>
2025-02-18 18:22:44 +0100

    update 18.2

--- src/models.py
+++ src/models.py
@@ -80,9 +80,6 @@ class ImageFingerPrint(nn.Module):
             nn.Conv2d(num_filters, num_filters * 2, kernel_size=3, stride=2, padding=1),
             nn.LeakyReLU(0.2),
 
-            nn.Conv2d(num_filters * 2, num_filters * 2, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-
             nn.Conv2d(num_filters * 2, num_filters * 4, kernel_size=3, stride=2, padding=1),
             nn.LeakyReLU(0.2),
 
@@ -96,7 +93,10 @@ class ImageFingerPrint(nn.Module):
 
         # 全连接层
         self.classifier = nn.Sequential(
-            nn.Linear(num_filters * 16 * (clip_width//64) * (clip_height//64), clip_width * (clip_height//256)),
+            nn.Linear(num_filters * 16 * (clip_width//32) * (clip_height//32), clip_width * (clip_height//32)),
+            nn.Tanh(),
+
+            nn.Linear(clip_width * (clip_height//32), clip_width * (clip_height // 256)),
             nn.Tanh()
         )
 
