b2d111f9b1d3c9ac1080498cab65247cacc1be25
Xinxiang <angelowxx@gmail.com>
2025-02-18 10:26:29 +0100

    update 17.2

--- src/train.py
+++ src/train.py
@@ -64,12 +64,12 @@ def train_example(rank, world_size, num_epochs):
     image_folder_path = os.path.join(os.getcwd(), 'data', 'train')
     train_data = ImageDatasetWithTransforms(image_folder_path, normalize_img_size, downward_img_quality)
     sampler = torch.utils.data.distributed.DistributedSampler(train_data, num_replicas=world_size, rank=rank)
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=16, sampler=sampler, num_workers=0)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=10, sampler=sampler, num_workers=0)
 
     if dist.get_rank() == 0:
         image_folder_path = os.path.join(os.getcwd(), 'data', 'val')
         val_data = ImageDatasetWithTransforms(image_folder_path, normalize_img_size, downward_img_quality)
-        val_loader = torch.utils.data.DataLoader(val_data, batch_size=16, num_workers=0)
+        val_loader = torch.utils.data.DataLoader(val_data, batch_size=10, num_workers=0)
 
     avg_losses = []
 
