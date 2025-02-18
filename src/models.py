868dc534a3b790e8c7a0f32e593ae795e94a79c6
Xinxiang <angelowxx@gmail.com>
2025-02-18 22:29:57 +0100

    update 18.2

--- src/models.py
+++ src/models.py
@@ -74,22 +74,22 @@ class ImageFingerPrint(nn.Module):
         self.model = nn.Sequential(
             # Input layer: (input_channels x H x W) -> (num_filters x H/2 x W/2)
             nn.Conv2d(input_channels, num_filters, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2),
 
             nn.Conv2d(num_filters, num_filters, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2),
 
             nn.Conv2d(num_filters, num_filters * 2, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2),
 
             nn.Conv2d(num_filters * 2, num_filters * 2, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2),
 
             nn.Conv2d(num_filters * 2, num_filters * 4, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2),
 
             nn.Conv2d(num_filters * 4, num_filters * 4, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.LeakyReLU(0.2),
 
         )
 
