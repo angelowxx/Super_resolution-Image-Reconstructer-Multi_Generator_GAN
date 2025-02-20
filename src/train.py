8df89e169e8283fd18c4b61bcc9566a8e6336cfa
Xinxiang <angelowxx@gmail.com>
2025-02-20 15:41:51 +0100

    update 19.2

--- src/train.py
+++ src/train.py
@@ -90,7 +90,7 @@ def train_example(rank, world_size, num_epochs, continue_training, prefix):
     train_data = ImageDatasetWithTransforms(train_folder_path, normalize_img_size, downward_img_quality)
 
     # Define split sizes (e.g., 70% train, 30% validation)
-    split_ratio = 0.08
+    split_ratio = 0.7
     train_size = int(split_ratio * len(train_data))
     val_size = len(train_data) - train_size
 
@@ -102,8 +102,8 @@ def train_example(rank, world_size, num_epochs, continue_training, prefix):
     val_sampler = DistributedSampler(val_subset, num_replicas=world_size, rank=rank)
 
     # Create DataLoaders
-    train_loader = DataLoader(train_subset, batch_size=7, sampler=train_sampler, num_workers=0)
-    val_loader = DataLoader(val_subset, batch_size=7, sampler=val_sampler, num_workers=0)
+    train_loader = DataLoader(train_subset, batch_size=8, sampler=train_sampler, num_workers=0)
+    val_loader = DataLoader(val_subset, batch_size=8, sampler=val_sampler, num_workers=0)
 
     psnrs = []
     ssims = []
