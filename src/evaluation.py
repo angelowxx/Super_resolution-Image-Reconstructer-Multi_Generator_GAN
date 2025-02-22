ba8476689afe08339799f8e514a387b836206290
Xinxiang <angelowxx@gmail.com>
2025-02-22 20:38:27 +0100

    update 21.2

--- src/evaluation.py
+++ src/evaluation.py
@@ -48,11 +48,11 @@ if __name__ == "__main__":
                                 default=os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                      '..', 'data', 'eval'),
                                 help='where the evaluation dataset stored')
-    cmdline_parser.add_argument('-l', '--lr_dir',
-                                default='LRbicx4',
+    cmdline_parser.add_argument('-lr', '--lr_dir',
+                                default='LOW x4 URban100 ',
                                 help='where low resolution images stored under eval dataset')
-    cmdline_parser.add_argument('-h', '--hr_dir',
-                                default='original',
+    cmdline_parser.add_argument('-hr', '--hr_dir',
+                                default='HIGH x4 URban100 ',
                                 help='where high resolution images stored under eval dataset')
 
     args, unknowns = cmdline_parser.parse_known_args()
