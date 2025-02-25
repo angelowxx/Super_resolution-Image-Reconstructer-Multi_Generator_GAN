2358ccba63f5309cec50db230bc488f19c1d48be
Xinxiang <angelowxx@gmail.com>
2025-02-25 23:46:31 +0100

    update

--- src/train.py
+++ src/train.py
@@ -77,7 +77,7 @@ def train_example(rank, world_size, num_epochs, continue_training, prefix):
     train_data = ImageDatasetWithTransforms(train_folder_path, normalize_img_size, downward_img_quality)
 
     # Define split sizes (e.g., 70% train, 30% validation)
-    split_ratio = 0.8
+    split_ratio = 0.7
     train_size = int(split_ratio * len(train_data))
     val_size = len(train_data) - train_size
 
