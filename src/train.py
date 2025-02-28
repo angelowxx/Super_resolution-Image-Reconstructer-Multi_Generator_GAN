afc45cef6c1b99ce14c74d15320934755f25021b
Xinxiang <angelowxx@gmail.com>
2025-02-28 20:38:29 +0100

    remove GAN

--- src/train.py
+++ src/train.py
@@ -268,7 +268,7 @@ def compute_score(model, val_loader, device):
     t = tqdm(val_loader, desc=f"validating:")
     cnt = 0
     for batch_idx, (hr_imgs, lr_imgs) in enumerate(t):
-        if cnt == 30:
+        if cnt == 50:
             break
         psnr = 0
         ssim = 0
