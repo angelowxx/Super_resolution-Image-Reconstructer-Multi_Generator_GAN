db928fc538051eafc3d666dd8c77a01d4ad6b3a7
Xinxiang <angelowxx@gmail.com>
2025-03-02 14:45:13 +0100

    update 0301

--- src/utils.py
+++ src/utils.py
@@ -207,7 +207,7 @@ class ReconstructionLoss(nn.Module):
         for i in range(1):
             edges = F.conv2d(edges, mean_filter, padding=1, groups=3)
 
-        edges = torch.clamp(self.normalize(edges, 1, 0.2), 0.8, 1.2)
+        edges = torch.clamp(self.normalize(edges, 1, 0.2), 0, 2)
 
         return edges
 
