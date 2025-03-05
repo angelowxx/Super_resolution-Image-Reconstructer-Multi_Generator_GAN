139a888ddc9b23d885182935dcc5ec4894dbe8c8
Xinxiang <angelowxx@gmail.com>
2025-03-05 02:32:57 +0100

    0305

--- src/train.py
+++ src/train.py
@@ -21,6 +21,8 @@ import torchvision.utils as vutils
 import torch.nn.functional as F
 
 nums_epoch = 5
+s_r = 0.01
+c_nums = 3
 warmUp_epochs = nums_epoch // 5
 
 
@@ -79,7 +81,7 @@ def train_example(rank, world_size, num_epochs, continue_training, prefix):
     val_subset = ImageDatasetWithTransforms(val_folder_path, normalize_img_size, downward_img_quality)
 
     # Define split sizes (e.g., 70% train, 30% validation)
-    split_ratio = 0.01
+    split_ratio = s_r
     train_size = int(split_ratio * len(train_data))
     val_size = len(train_data) - train_size
 
@@ -267,7 +269,7 @@ def compute_score(model, val_loader, device):
     t = tqdm(val_loader, desc=f"validating:")
     cnt = 0
     for batch_idx, (hr_imgs, lr_imgs) in enumerate(t):
-        if cnt == 3:
+        if cnt == c_nums:
             break
         psnr = 0
         ssim = 0
