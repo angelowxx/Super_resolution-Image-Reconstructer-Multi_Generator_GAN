fb4e5ed446476dd50b3fcfcfc3d1b8546912d247
Xinxiang <angelowxx@gmail.com>
2025-04-26 20:29:42 +0200

    0426

--- src/train.py
+++ src/train.py
@@ -195,7 +195,7 @@ def train_generator(generator, discriminator, lr_imgs, hr_imgs, vgg_extractor,
     com_loss = g_criterion(hr_imgs, sr_images)
     g_d_loss = loss_fn(fake_preds, torch.ones_like(fake_preds))
     # g_d_loss = torch.tensor(0)
-    g_loss = com_loss # + g_d_loss
+    g_loss = com_loss*10 + g_d_loss
 
     g_optimizer.zero_grad()
     g_loss.backward()
