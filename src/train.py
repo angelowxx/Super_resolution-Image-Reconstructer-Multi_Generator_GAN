c5618a633f1abe58e1473a255e39837d9ca04c36
Xinxiang <angelowxx@gmail.com>
2025-04-26 19:44:59 +0200

    0426, stop training the generator, to check the valid structure of discriminator

--- src/train.py
+++ src/train.py
@@ -55,11 +55,11 @@ def train_example(rank, world_size, num_epochs, continue_training, prefix):
     if continue_training:
         generator.load_state_dict(torch.load(os.path.join(os.getcwd(), 'results', f'{prefix}_generator_model_0.pth'),
                                              weights_only=True))
-        discriminator.load_state_dict(
-            torch.load(os.path.join(os.getcwd(), 'results', f'{prefix}_discriminator_model_0.pth'),
-                       weights_only=True))
-        lr_generator = lr_generator / 50
-        lr_dicriminator = lr_dicriminator / 50
+        # discriminator.load_state_dict(
+        #     torch.load(os.path.join(os.getcwd(), 'results', f'{prefix}_discriminator_model_0.pth'),
+        #                weights_only=True))
+        # lr_generator = lr_generator / 50
+        # lr_dicriminator = lr_dicriminator / 50
         prefix = "Post-Training"
 
     g_optimizer = optim.Adam(generator.parameters(), lr=lr_generator)
@@ -114,7 +114,7 @@ def train_example(rank, world_size, num_epochs, continue_training, prefix):
 
         lr_scheduler.step()
 
-        # d_lr_scheduler.step()
+        d_lr_scheduler.step()
 
         if (epoch + 1) % 5 == 0:
             validate(generator, val_loader, device, epoch, prefix, dist.get_rank())
@@ -158,15 +158,15 @@ def train_one_epoch(generator, train_loader, g_optimizer, vgg_extractor
         hr_imgs = hr_imgs.to(device)
         lr_imgs = lr_imgs.to(device)
 
-        # d_loss = train_discriminator(discriminator, generator, hr_imgs, lr_imgs, d_optimizer, loss_fn)
+        d_loss = train_discriminator(discriminator, generator, hr_imgs, lr_imgs, d_optimizer, loss_fn)
 
-        g_loss, com_loss, g_d_loss = train_generator(generator, discriminator, lr_imgs, hr_imgs, vgg_extractor,
-                                                     g_criterion, g_optimizer, loss_fn)
+        # g_loss, com_loss, g_d_loss = train_generator(generator, discriminator, lr_imgs, hr_imgs, vgg_extractor,
+        #                                              g_criterion, g_optimizer, loss_fn)
 
-        sum_g_loss += g_loss
-        # sum_d_loss += d_loss
-        sum_com_loss += com_loss
-        sum_g_d_loss += g_d_loss
+        # sum_g_loss += g_loss
+        sum_d_loss += d_loss
+        # sum_com_loss += com_loss
+        # sum_g_d_loss += g_d_loss
 
         t.set_postfix(g=sum_g_loss / (batch_idx + 1), d=sum_d_loss / (batch_idx + 1))
 
@@ -301,7 +301,7 @@ def compute_score(model, val_loader, device):
 
 
 if __name__ == "__main__":
-    continue_training = False
+    continue_training = True
     prefix = "Training"
 
     world_size = torch.cuda.device_count()
